bb7147a6bb3b2348b513f3ffef11dd68cec8e0fd
Kirubelgi <kirubelgirma174@gmail.com>
2025-02-10 23:20:14 +0300

    footer ubdated

--- client/src/components/common/Footer.jsx
+++ client/src/components/common/Footer.jsx
@@ -1,3 +1,4 @@
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 export default function Footer() {
@@ -29,10 +30,10 @@ export default function Footer() {
   }
   return (
 
-    <footer className="bg-gray-800 px-4 md:px-16 lg:px-28">
+    <footer className=" px-4 md:px-16 lg:px-28 ">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         <div>
-          <h2 className="text-lg font-bold mb-4">
+          <h2 className="text-lg font-bold mb-4 ">
             About Us
           </h2>
           <p className="text-gray-300">
@@ -40,7 +41,7 @@ export default function Footer() {
           </p>
         </div>
         <div>
-          <h2 className="text-lg font-boldmb-4">Quick Links</h2>
+          <h2 className="text-lg font-bold mb-4 ">Quick Links</h2>
           <ul>
             <li className="bg-transparent">
             <Link
@@ -63,7 +64,7 @@ export default function Footer() {
                       </li>
                       <li className="bg-transparent">
                       <Link
-                      className="text-md hover:text-accent bg-none bg-transparent"
+                      className=" hover:text-accent bg-none bg-transparent"
                       to={"/#WhyChooseUs"}
                       onClick={handleClickW}
                     >
@@ -73,11 +74,44 @@ export default function Footer() {
                       </li>
           </ul>
         </div>
-        <div></div>
+        <div>
+          <h2 className="text-lg font-bold mb-4 ">Follow Us</h2>
+          <ul className="flex space-x-4">
+            <li>
+              <FaFacebookF className="text-blue-500" />{" "}
+              <a
+                href=""
+                className="hover:underline text-gray-300"
+              >
+                Facebook
+              </a>
+            </li>
+            <li>
+              <FaTwitter className="text-sky-500" />{" "}
+              <a
+                href=""
+                className="hover:underline text-gray-300"
+              >
+                Twitter
+              </a>
+            </li>
+            <li>
+              <FaInstagram className="text-orange-500" />{" "}
+              <a
+                href=""
+                className="hover:underline text-gray-300"
+              >
+                Instagram
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <p>
+      <div className="border-t border-gray-600 p-4 text-gray-300 text-center mt-4">
+      <p >
         @ <span className="text-accent>">Chereta</span> {date}
       </p>
+      </div>
     </footer>
   );
 }
